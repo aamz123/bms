69174959ebbcdabefa82a4d7d37adda5900832dd
Kerem ERDEN <keremerden97@gmail.com>
2024-11-03 15:45:10 +0000

    update: vehicle view finished with batteries and sliders

--- bms/app/page.tsx
+++ bms/app/page.tsx
@@ -1,6 +1,15 @@
 import Image from "next/image";
 import BatteryIndicator from "./components/batteryindicator";
+import VehicleView from "./components/VehicleView";
 
 export default function Home() {
-  //return <div > <BatteryIndicator /></div>;
+  return (
+    <div>
+      <h1 className="text-2xl text-center h-[30px]">
+        Battery Management System - Team 4
+      </h1>
+      <VehicleView />{" "}
+    </div>
+    //return <div > <BatteryIndicator /></div>;
+  );
 }
